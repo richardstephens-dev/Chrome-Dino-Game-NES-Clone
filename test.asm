3236c9a2b4187499c5bdf9a3a894f39a1d414350
elencus <richard.stephens.15@ucl.ac.uk>
2023-01-14 18:06:51 +0000

    Back to basics tutorial

---
+++ test.asm
@@ -0,0 +1,168 @@
+; This is a demo project for learning 6502 Assembly for the NES.
+; The goal is a bottom-to-top platformer called "Ghost in Limbo".
+; Boilerplate template from https://github.com/NesHacker/DevEnvironmentDemo/blob/main/demo.s
+.segment "HEADER"
+  ;.byte "NES", $1A      ; iNES header identifier
+  .byte $4E, $45, $53, $1A
+  .byte 2               ; 2x 16KB PRG code
+  .byte 1               ; 1x  8KB CHR data
+  .byte $00, $00        ; mapper 0, mirroring: horizontal
+
+.segment "VECTORS"
+  ;; When an NMI happens (once per frame if enabled) the label nmi:
+  .addr nmi
+  ;; When the processor first turns on or is reset, it will jump to the label reset:
+  .addr reset
+  ;; External interrupt IRQ (unused)
+  .addr 0
+
+; "nes" linker config requires a STARTUP section, even if it's empty
+.segment "STARTUP"
+
+; Main code segment for the program
+.segment "CODE"
+
+reset:
+  sei		; disable IRQs
+  cld		; disable decimal mode
+  ldx #$40
+  stx $4017	; disable APU frame IRQ
+  ldx #$ff 	; Set up stack
+  txs		;  .
+  inx		; now X = 0
+  stx $2000	  ; disable NMI
+  stx $2001 	; disable rendering
+  stx $4010 	; disable DMC IRQs
+
+;; first wait for vblank to make sure PPU is ready
+vblankwait1:
+  bit $2002
+  bpl vblankwait1
+
+clear_memory:
+  lda #$00
+  sta $0000, x
+  sta $0100, x
+  sta $0200, x
+  sta $0300, x
+  sta $0400, x
+  sta $0500, x
+  sta $0600, x
+  sta $0700, x
+  inx
+  bne clear_memory
+
+;; second wait for vblank, PPU is ready after this
+vblankwait2:
+  bit $2002
+  bpl vblankwait2
+
+;; load palette
+main:
+load_palettes:
+  lda $2002     ; Read PPU status to reset address latch
+  lda #$3f      ; First palette address
+  sta $2006     ; Set PPU address lo byte of #3f00 (palette 0)
+  lda #$00      ; Start at 0
+  sta $2006     ; Set PPU address hi byte of #3f00 (palette 0)
+  ldx #$00      ; Start at 0
+@loop:
+  lda palettes, x
+  sta $2007     ; Write palette data
+  inx
+  cpx #$0c      ; 12 bytes per palette
+  bne @loop
+
+enable_rendering:
+  lda #%10000000	; Enable NMI
+  sta $2000
+  lda #%00010000	; Enable Sprites
+  sta $2001
+
+forever:
+  jmp forever
+
+nmi:
+  ldx #$00 	; Set SPR-RAM address to 0
+  stx $2003
+@loop:	lda ghost, x 	; Load the hello message into SPR-RAM
+  sta $2004
+  inx
+  cpx #$18
+  bne @loop
+  rti
+
+; Write the message to be displayed in the sprite memory
+ghost:
+  .byte $00, $00, $00, $00 	; Why do I need these here?
+  .byte $00, $00, $00, $00
+  .byte $6c, $00, $00, $6c
+  .byte $6c, $01, $00, $76
+  .byte $6c, $02, $00, $80
+  .byte $6c, $02, $00, $8A
+  .byte $6c, $03, $00, $94
+
+palettes:
+  ; Background Palette
+  .byte $0f, $00, $00, $00
+  .byte $0f, $00, $00, $00
+  .byte $0f, $00, $00, $00
+  .byte $0f, $00, $00, $00
+
+  ; Sprite Palette
+  .byte $0f, $20, $00, $00
+  .byte $0f, $00, $00, $00
+  .byte $0f, $00, $00, $00
+  .byte $0f, $00, $00, $00
+
+; Character memory
+.segment "CHARS"
+  .byte %11111111 ; G (00)
+  .byte %11111111
+  .byte %11000000
+  .byte %11011111
+  .byte %11011111
+  .byte %11000011
+  .byte %11111111
+  .byte %11111111
+  .byte $00, $00, $00, $00, $00, $00, $00, $00
+
+  .byte %11000011	; H (01)
+  .byte %11000011
+  .byte %11000011
+  .byte %11111111
+  .byte %11111111
+  .byte %11000011
+  .byte %11000011
+  .byte %11000011
+  .byte $00, $00, $00, $00, $00, $00, $00, $00
+
+  .byte %01111110	; O (02)
+  .byte %11100111
+  .byte %11000011
+  .byte %11000011
+  .byte %11000011
+  .byte %11000011
+  .byte %11100111
+  .byte %01111110
+  .byte $00, $00, $00, $00, $00, $00, $00, $00
+
+  .byte %11111111	; S (03)
+  .byte %11111111
+  .byte %11000000
+  .byte %11111111
+  .byte %11111111
+  .byte %00000011
+  .byte %11111111
+  .byte %11111111
+  .byte $00, $00, $00, $00, $00, $00, $00, $00
+
+  .byte %11111111	; T (04)
+  .byte %11111111
+  .byte %00011000
+  .byte %00011000
+  .byte %00011000
+  .byte %00011000
+  .byte %00011000
+  .byte %00011000
+  .byte $00, $00, $00, $00, $00, $00, $00, $00
